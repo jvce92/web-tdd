0259ad757e9d7f1777010d3939d185e649301d2f
jvce92 <jvce92@hotmail.com>
2016-02-10 13:42:24 -0300

    functionalTests.py - Tests for header info - Tests for user input

--- functionalTests.py
+++ functionalTests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class newVisitorTest(unittest.TestCase):
@@ -17,6 +18,19 @@ class newVisitorTest(unittest.TestCase):
 
         #checks for the website title
         self.assertIn('To-Do',self.browser.title)
+        headerText = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',headerText)
+        #user is asked to input its first to-do list
+        inputBox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputBox.get_attribute('placeholder'),'Enter a to-do item')
+        #test a new list input
+        inputBox.send_keys('Buy peacock feathers')
+        inputBox.send_keys(Keys.ENTER)
+        #check if the new entry is at the to-do list
+        table = self.browser.find_elements_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertEqual(any(row.tex == '1: Buy peacock feathers' for row in rows))
+
         self.fail('Finish the test!')
 
 
