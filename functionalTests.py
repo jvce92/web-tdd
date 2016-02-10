c6549ef8322206a7ed991e32b530ea7607516cee
jvce92 <jvce92@hotmail.com>
2016-02-10 02:27:03 -0300

    - The FT was detailed in the comments; - The FT uses unittest; - A test class was defined.

--- functionalTests.py
+++ functionalTests.py
@@ -1,6 +1,24 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class newVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def testPageTitle(self):
+
+        #Access the to-do list website
+        self.browser.get('http://localhost:8000')
+
+        #checks for the website title
+        self.assertIn('To-Do',self.browser.title)
+        self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
