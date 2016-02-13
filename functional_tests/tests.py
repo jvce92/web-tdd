f605fa8f8dc0b3cb974c51e21239d1943c64e003
jvce92 <jvce92@hotmail.com>
2016-02-13 12:26:09 -0300

    FT is ale to run at a stagint server

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,8 +2,23 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import sys
 
 class newVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return #
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -19,7 +34,7 @@ class newVisitorTest(StaticLiveServerTestCase):
 
     def testPageTitle(self):
         #Access the to-do list website
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         #checks for the website title
         self.assertIn('To-Do',self.browser.title)
@@ -42,7 +57,7 @@ class newVisitorTest(StaticLiveServerTestCase):
         #a new user shows up
         self.browser.quit()
         self.browser = webdriver.Firefox()
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         inputBox = self.browser.find_element_by_id('id_new_item')
         inputBox.send_keys('Buy milk')
@@ -57,7 +72,7 @@ class newVisitorTest(StaticLiveServerTestCase):
         self.assertIn('Buy milk', pageText)
 
     def testLayouAndStyling(self):
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024,768)
 
         inputBox = self.browser.find_element_by_id('id_new_item')
