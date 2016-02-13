1ce0b6f7a3f725e81e36a1296147c3f0d2e7e862
jvce92 <jvce92@hotmail.com>
2016-02-13 14:02:32 -0300

    new static folder

--- superlists/settings.py
+++ superlists/settings.py
@@ -100,16 +100,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR,'../static'))
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR,'/static'))
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, '/static/'),
 )
