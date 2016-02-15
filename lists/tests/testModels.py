63f1fbd8da3bee2d1820a8be3690d6ec1eeb3dfe
jvce92 <jvce92@hotmail.com>
2016-02-15 14:55:26 -0300

    Split out unit tests into two test files

---
+++ lists/tests/testModels.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+from lists.models import Item, List
+
+
+# Create your tests here.
+#
+# class smokeTest(TestCase):
+#
+#     def testBadMath(self):
+#         self.assertEqual(1+1,3)
+
+class ListAndItemModelsTest(TestCase):
+
+    def testSavingAndRetrievingItems(self):
+        myList = List()
+        myList.save()
+
+        firstItem = Item()
+        firstItem.text = 'The first list item'
+        firstItem.list = myList
+        firstItem.save()
+
+        secondItem = Item()
+        secondItem.text = 'The second list item'
+        secondItem.list = myList
+        secondItem.save()
+
+        savedItems = Item.objects.all()
+        self.assertEqual(savedItems.count(),2)
+
+        savedLists = List.objects.first()
+        self.assertEqual(savedLists, myList)
+
+        firstSavedItem = savedItems[0]
+        secondSavedItem = savedItems[1]
+        self.assertEqual(firstSavedItem.text,'The first list item')
+        self.assertEqual(secondSavedItem.text,'The second list item')
+        self.assertEqual(firstSavedItem.list, myList)
+        self.assertEqual(secondSavedItem.list, myList)
--- lists/tests/testView.py
+++ lists/tests/testView.py
@@ -25,35 +25,6 @@ class homePageTest(TestCase):
         expectedHtml = render_to_string('home.html')
         self.assertEqual(expectedHtml,response.content.decode())
 
-class ListAndItemModelsTest(TestCase):
-
-    def testSavingAndRetrievingItems(self):
-        myList = List()
-        myList.save()
-
-        firstItem = Item()
-        firstItem.text = 'The first list item'
-        firstItem.list = myList
-        firstItem.save()
-
-        secondItem = Item()
-        secondItem.text = 'The second list item'
-        secondItem.list = myList
-        secondItem.save()
-
-        savedItems = Item.objects.all()
-        self.assertEqual(savedItems.count(),2)
-
-        savedLists = List.objects.first()
-        self.assertEqual(savedLists, myList)
-
-        firstSavedItem = savedItems[0]
-        secondSavedItem = savedItems[1]
-        self.assertEqual(firstSavedItem.text,'The first list item')
-        self.assertEqual(secondSavedItem.text,'The second list item')
-        self.assertEqual(firstSavedItem.list, myList)
-        self.assertEqual(secondSavedItem.list, myList)
-
 class ListViewTest(TestCase):
 
     def testUsesListTemplate(self):
